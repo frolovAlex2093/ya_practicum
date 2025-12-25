df2c6a9a8f0c986d70777b54a31f8326a200aa4a
frolovAlex2093 <frolov.alex2093@gmail.com>
2025-12-25 15:19:03 +0300

    delete transaction from dao

--- src/main/java/ru/blog/dao/impl/CommentDaoImpl.java
+++ src/main/java/ru/blog/dao/impl/CommentDaoImpl.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-@Transactional
 public class CommentDaoImpl implements CommentDao {
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
--- src/main/java/ru/blog/dao/impl/PostDaoImpl.java
+++ src/main/java/ru/blog/dao/impl/PostDaoImpl.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-@Transactional
 public class PostDaoImpl implements PostDao {
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
